cbd7a6b92c80865871d65dcef007eee168c698c2
madiedibble <115160744+madiedibble@users.noreply.github.com>
2022-10-27 16:12:01 +0100

    Add files via upload

---
+++ simple-audio-stub-main (1)/simple-audio-stub-main/js/simple-player.js
@@ -0,0 +1,89 @@
+console.log("hello-world!")
+const currentTime = document.getElementById('current-time');
+const totalTime = document.getElementById('total-time');
+const playPauseButton = document.getElementById('play-pause-button');
+const seekBar = document.getElementById('seek-bar');
+const audio = new Audio("audio/Soft-Background-for-Interview.webm");
+let isSeeking = false; 
+
+playPauseButton.onclick = function () {
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+    }
+}
+
+//AUDIO LISTENERS
+//event triggered pnce audio loaded
+
+audio.oncanplaythrough = function () {
+    seekBar.disabled = false;
+}
+
+// event triggered when audio plays
+audio.onplay = function () {
+    playPauseButton.src = "images/pause.svg";
+}
+
+
+//event triggered when audio unpaused
+audio.onpause = function(){
+    playPauseButton.src = "images/play.svg";
+}
+//event triggered on meta data load
+audio.onloadedmetadata = function(){
+    totalTime.innerHTML = formatTime (audio.duration);
+    currentTime.innerHTML = formatTime (0);
+    seekBar.max = Math.floor(audio.duration);
+}
+
+//event triggered when time updates
+
+audio.ontimeupdate = function(){
+    currentTime.innerHTML = formatTime(audio.currentTime)
+    if (!isSeeking){
+        seekBar.value = Math.floor(audio.currentTime)
+    }
+
+}
+//seek bar listeners
+
+seekBar.oninout = function(){
+    isSeeking = true;
+}
+
+// evernt triggered when seekbar is manually changed manually
+seekBar.onchange = function(){
+    audio.currentTime = seekBar.value;
+    isSeeking = false;
+}
+
+//event triggered when audio ends
+audio.onended = function(){
+    currentTime.innerHTML = formatTime(0);
+    seekBar.value = 0
+    playPauseButton.src = "images/play.svg";
+}
+
+// UTILITY FUNCTIONS
+// takes total seconds (number) and returns a formatted string 
+function formatTime(secs) {
+    let hours = Math.floor(secs / 3600);
+    let minutes = Math.floor((secs - (hours * 3600)) / 60);
+    let seconds = Math.floor((secs - (hours * 3600)) - minutes * 60);
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+    if (hours > 0) {
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+        return hours + ":" + minutes + ":" + seconds;
+    } else {
+        return minutes + ":" + seconds;
+    }
+}
